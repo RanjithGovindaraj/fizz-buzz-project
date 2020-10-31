9c5facbab0756afec15a3f5742a237e8d9347281
RanjithGovindaraj <kmgrranjith@gmail.com>
2020-10-31 11:27:39 +0530

    commiting on branch 2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,14 +2,13 @@
 # multiples of 5 => BUZZ
 # multiples of 3 and 5 => FIZZBUZZ
 
-# First Approach
+# Second Approach
 for i in range(1,101):
-    if i % 3 == 0 and i % 5 ==0:
-        print("FIZZBUZZ")
-    elif i % 3 == 0:
-        print("FIZZ")
-    elif i % 5 == 0:
-        print("BUZZ")
-    else:
+    t = ""
+    if i % 3 == 0:
+        t+="FIZZ"
+    if i % 5 == 0:
+        t+="BUZZ"
+    if t == "":
         print(str(i))
 print("END")
