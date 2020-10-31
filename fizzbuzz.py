cde06787793555cbad524bfc1e48c84ab798f5fc
RanjithGovindaraj <kmgrranjith@gmail.com>
2020-10-31 11:19:53 +0530

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+# multiples of 3 => FIZZ
+# multiples of 5 => BUZZ
+# multiples of 3 and 5 => FIZZBUZZ
+
+# First Approach
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 ==0:
+        print("FIZZBUZZ")
+    elif i % 3 == 0:
+        print("FIZZ")
+    elif i % 5 == 0:
+        print("BUZZ")
+    else:
+        print(str(i))
